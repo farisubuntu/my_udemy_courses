eb12352a9735e32283db86b381f2d71ecddc9fb8
farisubuntu <electricalfaris@gmail.com>
2021-01-29 20:03:10 +0300

    adding jekyll.mm - a freeplane file

---
+++ Jekyll.mm
@@ -0,0 +1,112 @@
+<map version="1.1.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000080" CREATED="1611935527952" ID="ID_1164545233" MODIFIED="1611935672618" TEXT="Jekyll">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node COLOR="#990000" CREATED="1611935557827" ID="ID_1422544204" MODIFIED="1611939286344" POSITION="right" TEXT="Setup">
+<font BOLD="true" NAME="SansSerif" SIZE="13"/>
+<node CREATED="1611935897442" ID="ID_1510029765" MODIFIED="1611936116006" TEXT="Prerequests">
+<icon BUILTIN="desktop_new"/>
+<node CREATED="1611935941175" ID="ID_1436861756" MODIFIED="1611939264848" STYLE="bubble" TEXT="First, install Ruby">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1611936122333" ID="ID_1770326009" MODIFIED="1611936142327" TEXT="install jekyll">
+<node COLOR="#990000" CREATED="1611936144490" ID="ID_593167089" MODIFIED="1611939227287" TEXT=" $ gem install jekyll bundler">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1611936269432" ID="ID_791110905" MODIFIED="1611936293166" TEXT="create a new file">
+<node CREATED="1611936293168" ID="ID_1314721961" MODIFIED="1611939229926" TEXT="$ bundle init">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1611936314208" ID="ID_1175755950" MODIFIED="1611939231990" TEXT="append: gem &quot;jekyll&quot; Gemfile">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1611936402604" ID="ID_1928439596" MODIFIED="1611936420748" TEXT="install jekyll for your project">
+<node CREATED="1611936422168" ID="ID_1477652243" MODIFIED="1611939223768" TEXT="$ bundle">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1611936437780" ID="ID_480680399" MODIFIED="1611936576865" TEXT="Create a Site Directory">
+<node CREATED="1611936448052" ID="ID_1418078116" MODIFIED="1611939236181" TEXT="Create a new directory for your site - this will be called: the root dir">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1611936580822" ID="ID_141116202" MODIFIED="1611936610362" TEXT="insite the directory - add the first file">
+<node CREATED="1611936610364" ID="ID_378425498" MODIFIED="1611939274907" TEXT="$ touch index.html">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1611936622321" ID="ID_1294114300" MODIFIED="1611936665479">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <pre http-equiv="content-type" content="text/html; charset=utf-8" class="highlight"><code>&lt;!DOCTYPE html&gt;
+&lt;html&gt;
+  &lt;head&gt;
+    &lt;meta charset=&quot;utf-8&quot;&gt;
+    &lt;title&gt;Home&lt;/title&gt;
+  &lt;/head&gt;
+  &lt;body&gt;
+    &lt;h1&gt;Hello World!&lt;/h1&gt;
+  &lt;/body&gt;
+&lt;/html&gt;</code></pre>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1611936675850" ID="ID_1120624667" MODIFIED="1611937578624" TEXT="Build">
+<node CREATED="1611939142384" ID="ID_1282354219" MODIFIED="1611939216598" TEXT="$ jekyll build - outputs a static site to a folder called _site">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1611939301395" ID="ID_706810900" MODIFIED="1611939507675">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <code class="terminal">$ jekyll serve --livereload</code>&#160;=&gt; build and serve on a local web server plus live update
+    </p>
+  </body>
+</html>
+</richcontent>
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1611939519424" ID="ID_510776356" MODIFIED="1611939548328" TEXT="use your browser and go to:">
+<node CREATED="1611939548330" ID="ID_17986107" MODIFIED="1611939562975" TEXT="http://127.0.01:4000"/>
+</node>
+</node>
+<node CREATED="1611935565092" ID="ID_1340521432" MODIFIED="1611939288031" POSITION="right" TEXT="Liquid">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1611935614268" ID="ID_1607801081" MODIFIED="1611939289743" POSITION="right" TEXT="Front Matter">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1611935622043" ID="ID_814201182" MODIFIED="1611939288655" POSITION="right" TEXT="Layouts">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1611935625643" ID="ID_651868435" MODIFIED="1611939290399" POSITION="right" TEXT="Includes">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1611935629738" ID="ID_1590690472" MODIFIED="1611939291935" POSITION="right" TEXT="Data Files">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1611935635641" ID="ID_1937749418" MODIFIED="1611939291390" POSITION="right" TEXT="Assets">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1611935643063" ID="ID_1380946677" MODIFIED="1611939292749" POSITION="right" TEXT="Blogging">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1611935650174" ID="ID_1104550325" MODIFIED="1611939294029" POSITION="right" TEXT="Collections">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1611935653493" ID="ID_1259776834" MODIFIED="1611939293381" POSITION="right" TEXT="Deployment">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+</map>
